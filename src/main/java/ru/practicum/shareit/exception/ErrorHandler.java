1543b1e2f3ba6b648bb2917e53608a52ea22aedf
Mazhanov <mazhanov1997@yandex.ru>
2023-02-24 01:09:24 +0300

    Pravki2

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -9,10 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.item.OtherUserException;
 import ru.practicum.shareit.user.EmailDuplicateException;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
@@ -52,8 +48,6 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(final Exception exception) {
         log.info("500 {}", exception.getMessage(), exception);
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        exception.printStackTrace(new PrintStream(out));
-        return new ErrorResponse(out.toString(StandardCharsets.UTF_8));
+        return new ErrorResponse(exception.getMessage());
     }
 }
